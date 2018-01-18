6f24cf67c3ba5a0490af1eaf203d4b16b0c8ac32
Christopher Borchert <christopher.borchert@intradesys.com>
2018-01-18 14:01:14 +0100

    Gulp template compilation implemented.

--- deraner/public/templates/Ulmenstein/assets/js/app/run.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-window.onload = function (e) {
-    alert('Hallo Welt is here!');
-};
--- gulpfile.js
+++ gulpfile.js
@@ -2,81 +2,223 @@
 // Include gulp
 const gulp = require('gulp');
 
+const minify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+
 const pug = require('gulp-pug');
 const gulpPugBeautify = require('gulp-pug-beautify');
 
 const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 
-gulp.task('pug', () => {
-    return gulp.src('deraner/templates/**/*.pug')
-        .pipe(pug())
-        .pipe(gulpPugBeautify({ omit_empty: true }))
-        .pipe(gulp.dest('deraner/public/templates'))
-        ;
+const { lstatSync, readdirSync, existsSync, readFileSync } = require('fs');
+const { join } = require('path');
+
+const isDirectory = source => lstatSync(source).isDirectory()
+const getDirectories = source =>
+    readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+const templatesSrcDir = 'deraner/templates';
+const templatesDestDir = 'deraner/public/templates';
+const templatesSettingsFile = 'template.json';
+
+let templates = null;
+const forEachTemplate = callable => {
+    if(templates === null) {
+        templates = new Array();
+        let dirs = getDirectories(templatesSrcDir);
+        for(let i = 0; i < dirs.length; i++) {
+            let path = dirs[i];
+            let tplPath = path + '/' + templatesSettingsFile;
+            if(existsSync(tplPath)) {
+                let tpl = JSON.parse(readFileSync(tplPath, 'utf8'));
+                let dpath = templatesDestDir + '/' + tpl.name;
+                callable(tpl, path, dpath);
+                templates.push({
+                    path : path,
+                    tpl : tpl,
+                    dpath : dpath
+                });
+            }
+        }
+    } else {
+        for(let i = 0; i < templates.length; i++) {
+            var t = templates[i];
+            callable(t.tpl, t.path, t.dpath);
+        }
+    }
+};
+
+const builders = {
+    sass    : sass,
+    scss    : sass,
+    babel   : babel,
+    pug     : pug,
+    jade    : pug
+};
+
+const parseDestData = dest => {
+    let idx = dest.indexOf(':');
+
+    let dir = dest;
+    let file = null;
+
+    if(idx >= 0) {
+        dir = dest.substr(0, idx);
+        file = dest.substr(idx + 1);
+    }
+
+    return {
+        path : dir,
+        file : file
+    };
+};
+
+const compileAssets = (tpl, path, dpath) => {
+    let assets = tpl.assets || null;
+    if(assets === null) return;
+
+    let rets = new Array();
+
+    for(let dest in assets) {
+        let streams = new Array();
+        let pipe = assets[dest];
+
+        let dst = parseDestData(dest);
+
+        for(let i = 0; i < pipe.length; i++) {
+            let args = pipe[i];
+
+            let fullSRC = new Array();
+
+            if('src' in args) {
+                if(typeof args.src == 'string') {
+                    fullSRC.push(path + '/assets/' + args.src);
+                } else {
+                    for(let i = 0; i < args.src.length; i++) {
+                        fullSRC.push(path + '/assets/' + args.src[i]);
+                    }
+                }
+            } else {
+                console.error('Compilation error! Missing src parameter.' + dest + ' could not be created.')
+                continue;
+            }
+
+            let stream = null;
+
+            if('builder' in args) {
+                if(args.builder in builders) {
+                    if('options' in args) {
+                        stream = gulp.src(fullSRC).pipe(builders[args.builder](args.options));
+                    } else {
+                        stream = gulp.src(fullSRC).pipe(builders[args.builder]());
+                    }
+                } else {
+                    console.error('Compilation error! Unknown builder ' + args.builder + '. ' + dest + ' could not be created.');
+                    stream = null;
+                }
+            } else {
+                stream = gulp.src(fullSRC);
+            }
+
+            if(stream !== null) {
+                streams.push(stream);
+            }
+        }
+
+        let stm = null;
+
+        if(dst.file !== null) {
+            stm = merge(...streams).pipe(concat(dst.file)).pipe(gulp.dest(dpath + '/assets/' + dst.path));
+        } else {
+            stm = merge(...streams).pipe(gulp.dest(dpath + '/assets/' + dst.path));
+        }
+
+        rets.push(stm);
+    }
+
+    return rets;
+};
+
+const compileTemplates = (tpl, path, dpath) => {
+    let templates = tpl.templates || null;
+    if(templates === null) return;
+
+    let rets = new Array();
+
+    for(let dest in templates) {
+        let streams = new Array();
+        let pipe = templates[dest];
+
+        let dst = parseDestData(dest);
+
+        for(let i = 0; i < pipe.length; i++) {
+            let args = pipe[i];
+
+            let fullSRC = new Array();
+
+            if('src' in args) {
+                if(typeof args.src == 'string') {
+                    fullSRC.push(path + '/' + args.src);
+                } else {
+                    for(let i = 0; i < args.src.length; i++) {
+                        fullSRC.push(path + '/' + args.src[i]);
+                    }
+                }
+            } else {
+                console.error('Compilation error! Missing src parameter.' + dest + ' could not be created.')
+                continue;
+            }
+
+            let stream = null;
+
+            if('builder' in args) {
+                if(args.builder in builders) {
+                    if('options' in args) {
+                        stream = gulp.src(fullSRC).pipe(builders[args.builder](args.options));
+                    } else {
+                        stream = gulp.src(fullSRC).pipe(builders[args.builder]());
+                    }
+                } else {
+                    console.error('Compilation error! Unknown builder ' + args.builder + '. ' + dest + ' could not be created.');
+                    stream = null;
+                }
+            } else {
+                stream = gulp.src(fullSRC);
+            }
+
+            if(stream !== null) {
+                streams.push(stream);
+            }
+        }
+
+        let stm = null;
+
+        if(dst.file !== null) {
+            stm = merge(...streams).pipe(concat(dst.file)).pipe(gulp.dest(dpath + '/' + dst.path));
+        } else {
+            stm = merge(...streams).pipe(gulp.dest(dpath + '/' + dst.path));
+        }
+
+        rets.push(stm);
+    }
+
+    return rets;
+};
+
+gulp.task('assets', () => {
+    forEachTemplate((tpl, path, dpath) => {
+        console.log('Compiling assets for template ' + tpl.name + '...');
+        compileAssets(tpl, path, dpath);
+    });
 });
 
-gulp.task('css', () => {
-    return gulp.src('deraner/templates/**/assets/css/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('deraner/public/templates'))
-    ;
+gulp.task('templates', () => {
+    forEachTemplate((tpl, path, dpath) => {
+        console.log('Compiling template ' + tpl.name + '...');
+        compileTemplates(tpl, path, dpath);
+    });
 });
 
-gulp.task('js', () => {
-    return gulp.src('deraner/templates/**/assets/js/**/*.js')
-            .pipe(babel({
-                presets: ['env']
-            }))
-            .pipe(gulp.dest('deraner/public/templates'))
-    ;
-});
-
-gulp.task('fonts', () => {
-    return gulp.src('deraner/templates/**/assets/css/webfonts/**/*')
-        .pipe(gulp.dest('deraner/public/templates'));
-});
-
-gulp.task('default', ['pug', 'css', 'js', 'fonts']);
-
-/*
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-// Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('deraner/Ulmenstein/assets/css/font-awesome/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('deraner/public/css'));
-});
-
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
-});
-
-// Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
-*/
+gulp.task('default', ['templates', 'assets']);
