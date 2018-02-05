41b658779e17fda1618dd69818a98b6b28aec98d
Viktor Machnik <viktor.machnik@app-logik.de>
2018-02-05 17:47:41 +0100

    Background

--- deraner/templates/Ulmenstein/assets/js/app/run.js
+++ deraner/templates/Ulmenstein/assets/js/app/run.js
@@ -1,3 +1,22 @@
-window.onload = e => {
-    console.log('Hallo Welt is here!');
-};
+const HeaderVue = new Vue({
+    el: 'head title',
+    data: {
+        title : 'Deraner'
+    },
+    computed: {
+    },
+    methods: {
+    }
+});
+
+
+const BodyVue = new Vue({
+    el: 'body main',
+    data: {
+
+    },
+    computed: {
+    },
+    methods: {
+    }
+});
